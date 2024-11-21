2809446a8a1479f2383f833612715712849571b0
Dlir <dinhlinh.work24@gmail.com>
2024-11-21 19:05:22 +0700

    change port, use cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('Base backend')
     .setDescription('The base API description')
@@ -21,6 +23,6 @@ async function bootstrap() {
     transform: true
   }))
   
-  await app.listen(3000);
+  await app.listen(3002);
 }
 bootstrap();
